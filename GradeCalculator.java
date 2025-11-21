517ed2e2b8a3a9f4628d4bd2d6c5b8499b096000
tejaswinis-source <tejaswinis1008@gmail.com>
2025-11-21 22:47:42 +0530

    Added GradeCalculator.java

---
+++ GradeCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class GradeCalculator {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter number of subjects: ");
+    int n = sc.nextInt();
+    int total = 0;
+    for(int i=1; i<=n; i++) {
+      System.out.priintln("Enter marks for subjects " + i + ": ");
+      int marks = sc.nextInt();
+      total += marks;
+    }
+    double average = total/(double) n;
+    char grade;
+    if(average >=90) 
+     grade = 'A';
+    else if(average >= 75)
+      grade = 'B';
+    else if(average >= 60)
+      grade = 'C';
+    else if(average >= 40)
+      grade = 'D';
+    else 
+    grade = 'F';
+  System.out.println("Total Marks: " + total);
+  System.out.println("Average: " + average);
+  System.out.println("Grade: " + grade);
+    sc.close();
+  }
+}
